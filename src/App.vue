2a3a6cc0ead507c89e0e6349331703a7eb02f49f
Aron Tolentino <arontolentino@outlook.com>
2019-10-03 12:45:37 -0400

    Create initial total amount containers

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,95 @@
 <template>
   <div id="app">
+    <div class="header">
+      <div class="totalSavings">
+        <h2>Total Savings</h2>
+        <p>$7,467.25</p>
+      </div>
+    </div>
+    <div class="input"></div>
+    <div class="transactions">
+      <div class="wrapper">
+        <div class="income">
+          <div class="totalIncome">
+            <h2>Total Income</h2>
+            <p>$9,148.20</p>
+          </div>
+          <div class="incomeTransactions"></div>
+        </div>
+        <div class="expenses">
+          <div class="totalExpenses">
+            <h2>Total Expenses</h2>
+            <p>$1,680.96</p>
+          </div>
+          <div class="expenseTransactions"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f5f5f5;
+}
+
+.wrapper {
+  max-width: 870px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+  background-color: #3827b4;
+  color: #fff;
+}
+
+.totalSavings {
+  text-align: center;
+}
+
+.transactions {
+  padding-top: 40px;
+}
+
+.income,
+.expenses {
+  width: 405px;
+  text-align: center;
+}
+
+.totalIncome,
+.totalExpenses {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90px;
+  background-color: #813baf;
+  color: #fff;
+  border-radius: 20px;
+}
 </style>
